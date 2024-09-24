816e8f278c04df86b2fe8a94be58d4a99bb84200
Daniar <zhylangozov.daniar@gmail.com>
2024-09-24 16:45:02 -0400

    5th commit - spotify csv parsed correctly, eslint clear

--- src/report.js
+++ src/report.js
@@ -1,1 +1,36 @@
 // report.js
+import { parse } from 'csv-parse';
+import * as fs from 'fs';
+import * as hoffy from '../src/hoffy.js';
+
+let records;
+fs.readFile('data/spotify-2023.csv', 'utf8', (err, data) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        parse(data, {}, (err, parsedData) => {
+            if (err){
+                console.log(err);
+            }
+            else{
+                // console.log(parsedData);
+
+                const dataObj = {
+                    'headers': parsedData[0],
+                    'rows': parsedData.slice(1)
+                };
+
+                records = hoffy.rowsToObjects(dataObj);
+                
+                records = records.map((elem) => {
+                    return hoffy.stringFieldToList(elem, 'artist(s)_name');
+                });
+
+                console.log(records);
+                
+            }
+        });
+    }
+});
+
